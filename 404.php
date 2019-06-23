6b849c41536262291b1f8e48c9f87d3cf525915e
Brian Beal <brianwbeal@gmail.com>
2019-06-23 14:47:35 -0700

    create 404 error page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+
+<div class="container error-screen">
+  <h1>404</h1>
+  <p>sorry, there's nothing here.</p>
+  <a href="/" class="btn btn-secondary mt-2">return home</a>
+</div>
+
+
+<?php get_footer(); ?>
